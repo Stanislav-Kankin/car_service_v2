81059b5ef71c34ecdb83c8ce1c6d10f4f167d75a
Stnislav-Kankin <stanislav.kankin@yandex.ru>
2026-01-22 15:10:01 +0300

    импорт забыл

--- webapp/app/routers/pages_public.py
+++ webapp/app/routers/pages_public.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.responses import HTMLResponse, RedirectResponse, Response
 import httpx
 import json
 
